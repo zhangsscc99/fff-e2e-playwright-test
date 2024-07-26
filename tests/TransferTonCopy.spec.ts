1d98931a6c4eabd8b6a8f2519fa77bd3f8e37f36
Yuting Wang <zhangsscc99@126.com>
2024-07-26 15:50:27 +0800

    transfer Ton

--- tests/TransferTonCopy.spec.ts
+++ tests/TransferTonCopy.spec.ts
@@ -61,10 +61,10 @@ test('test', async ({ context2 }) => {
 
     await tonkeeperNewPage2.close();
 
-    // const [newPage3] = await Promise.all([
-    //     context2.waitForEvent('page'),  
-    //     await homePage.clickSubmit(),
-    // ]);
+    const [newPage3] = await Promise.all([
+        context2.waitForEvent('page'),  
+        await homePage.clickSubmit(),
+    ]);
 
     // await newPage3.click('button.sc-Gqece.cXPFCG');
     // const inputSelector = 'input[type="password"].sc-hJJSeN.ixSRbR';
