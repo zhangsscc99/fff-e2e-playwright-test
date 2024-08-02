3f437641c4568c74a350329f3219465b2fd8159b
Yuting Wang <zhangsscc99@126.com>
2024-08-02 15:03:30 +0800

    referral

--- fixtures.ts
+++ fixtures.ts
@@ -13,7 +13,7 @@ export const test = base.extend<{
 }>({
     context1: async ({}, use) => {
         const pathToExtension = path.join(__dirname, 'assets/Extensions/omaabbefbmiijedngplfjmnooppbclkk/3.13.0_0');
-        const userDataDir = '../assets/Profile_test/Profile10';
+        const userDataDir = '../assets/Profile_test/Profile 1 backup';
         const browserContext = await chromium.launchPersistentContext(userDataDir, {
             headless: false,
             permissions: ['clipboard-read', 'clipboard-write'],
@@ -27,7 +27,7 @@ export const test = base.extend<{
     },
     context2: async ({}, use) => {
         const pathToExtension = path.join(__dirname, 'assets/Extensions/omaabbefbmiijedngplfjmnooppbclkk/3.13.0_0');
-        const userDataDir = '../assets/Profile_test/Profile100'; 
+        const userDataDir = '../assets/Profile_test/Profile 2 backup'; 
         const browserContext = await chromium.launchPersistentContext(userDataDir, {
             headless: false,
             permissions: ['clipboard-read', 'clipboard-write'],
